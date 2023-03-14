244b39a68e918c8357d336b3e80fcf7fba62fbea
TYHsueh <cheryl.ty.hsueh@gmail.com>
2023-03-13 22:48:27 -0700

    Assognment: putting it together

--- week1/my-first-react/src/components/PersonCard.js
+++ week1/my-first-react/src/components/PersonCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 const PersonCard = (props) =>{
     //set props {firstName:"",
@@ -7,12 +7,15 @@ const PersonCard = (props) =>{
     //           hair:""
     //}
     const {firstName, lastName, age, hair} = props;
+    //useState code:
+    const [stateAge, setStateAge] = useState(age);
     
     return(
         <div>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {stateAge}</p>
             <p>Hair Color: {hair}</p>
+            <button onClick = {() => setStateAge(stateAge +1)}>Birthday for {firstName} {lastName}</button>
         </div>
     )
 }
