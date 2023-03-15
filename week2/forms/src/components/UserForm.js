726898c653356251a80c629a5f2cfaa25cb692d5
TYHsueh <cheryl.ty.hsueh@gmail.com>
2023-03-15 13:37:29 -0700

    Assignment-hook form

--- week2/forms/src/components/UserForm.js
+++ week2/forms/src/components/UserForm.js
@@ -8,6 +8,24 @@ const UserForm =(props) =>{
     const [password, setPassword] = useState("");
     const [comfirmPassword, setComfirmPassword] = useState("");
 
+      // using an object to hold style options for our results div
+    const formDataDivStyle = {
+    textAlign: "center", 
+    width: "450px", 
+    margin: "auto",
+    border: "1px solid darkgrey",
+    }
+
+    const inputDataDivStyle = {
+    borderRadius: "5px",
+    backgroundColor: "#f2f2f2",
+    border: "1px solid darkgrey",
+    padding: "0px 10px",
+    margin: "5px",
+    width: "450px", 
+    }
+
+
     //use these data to create a user
     const createUser = (e) =>{
         // we must prevent the default refresh of the browser to keep our state from being reset
@@ -32,33 +50,33 @@ const UserForm =(props) =>{
 
     //return show the format/form to be shown on App.js(DOM)
     return (
-        <div>
-        <form onSubmit = {createUser}>
+        <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+        <form onSubmit = {createUser} style={{margin: "20px"}}>
             <h1>Hook Form</h1>
-            <div>
+            <div style={inputDataDivStyle}>
                 <label htmlFor = "firstName">First Name: </label>
-                <input type="text" name = "firstName" onChange={(e)=> setFirstName(e.target.value)} />
+                <input type="text" name = "firstName" onChange={(e)=> setFirstName(e.target.value)} value={firstName}/>
             </div>
-            <div>
+            <div style={inputDataDivStyle}>
                 <label htmlFor ="lastName">Last Name: </label>
                 <input type="text" name ="lastName" onChange={(e)=> setLastName(e.target.value)} />
             </div>
-            <div>
+            <div style={inputDataDivStyle}>
                 <label htmlFor = "email">Email: </label>
                 <input type="text" name = "email" onChange={(e)=> setEmail(e.target.value)} />
             </div>
-            <div>
+            <div style={inputDataDivStyle}>
                 <label htmlFor="password">Password: </label>
                 <input type="password" name="password" onChange={(e)=> setPassword(e.target.value)} />
             </div>
-            <div>
+            <div style={inputDataDivStyle}>
                 <label htmlFor ="comfirmPassword">Comfirm Password: </label>
                 <input type="password" name="comfirmPassword" onChange={(e)=> setComfirmPassword(e.target.value)} />
             </div>
             <input type="submit" value="Create User" />
         </form>
 
-        <div>
+        <div style={formDataDivStyle}>
             <h2>Hook Form Data</h2>
             <p>First Name: {firstName}</p>
             <p>Last Name: {lastName}</p>
